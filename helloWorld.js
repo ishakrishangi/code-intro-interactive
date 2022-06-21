0b9b819deda258447e25b36e3464d629e5a14c4b
ishakrishangi <krishangik@gmail.com>
2022-06-21 18:21:51 -0400

    intro code interactive lecurenote

---
+++ helloWorld.js
@@ -0,0 +1,40 @@
+console.log('Hello, Universe')
+
+const firstName = 'Isha';
+
+let age = 23;
+
+const isCool = true;
+
+let height;
+
+let weight = null;
+
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof isCool);
+// console.log(typeof height);
+// console.log(typeof weight);
+
+let x = 4;
+let y = 10;
+let z = 7 + 9;
+let sum = x + y + z;
+let diff = ((x + y) - (y + z));
+console.log(diff);
+
+let welcomeString = `Hello Universe ${firstName}`;
+console.log(welcomeString);
+
+if (age > 21){
+    console.log(`you can drink ${firstName}`)
+} else{
+    console.log('youve got some growing up to do');
+}
+
+let score = 78
+let passing = 40
+
+if (score < passing){
+    console.log("You failed")
+} else {console.log("You passed!")}
